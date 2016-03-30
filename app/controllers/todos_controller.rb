48f6b3b315372931de3fe7d512dc1142dacdc6bf
patrickmontalto <pmontalto@gmail.com>
2016-03-30 10:55:31 -0400

    Extract session[:current_email]

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,7 +2,7 @@ class TodosController < ApplicationController
   before_filter :authenticate
   
   def index
-    @todos = Todo.where(email: session[:current_email])
+    @todos = Todo.where(email: current_email)
   end
 
   def new
@@ -10,7 +10,7 @@ class TodosController < ApplicationController
   end
 
   def create
-    todo = Todo.new(todo_parameters.merge(email: session[:current_email]))
+    todo = Todo.new(todo_parameters.merge(email: current_email))
     todo.save
     redirect_to todos_path
   end
